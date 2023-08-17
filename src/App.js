319a214df977fbf9fb3696cb2248793d5e25232d
Rola Alasmar <rola.alasmar@perficient.com>
2023-08-17 12:44:57 -0500

    react todo app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react"
+import './style.css';
+import { NewToDoForm } from "./NewTodoForm";
+import { TodoList } from "./TodoList";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default function App(){
+  const [todos, setTodos] = useState(() => {
+    const localValue = localStorage.getItem("ITEMS")
+    if (localValue == null) return []
+
+    return JSON.parse(localValue)
+  })
+
+  useEffect(() => {
+    localStorage.setItem("ITEMS", JSON.stringify(todos))
+  }, [todos])
+
+
+function addTodo(title){
+  setTodos(currentTodos => {
+    return [
+      ...currentTodos,
+      {id: crypto.randomUUID(), title, completed: false},
+    ]
+  })
 }
 
-export default App;
+function toggleTodo(id, completed){
+  setTodos(currentTodos => {
+    return currentTodos.map(todo => {
+      if (todo.id === id){
+        return {...todo, completed}
+      }
+
+      return todo
+    })
+  })
+}
+
+function deleteTodo(id){
+  setTodos(currentTodos => {
+    return currentTodos.filter(todo => todo.id !== id)
+  })
+}
+
+  return (
+    <>
+    <NewToDoForm onSubmit={addTodo} />
+    <h1 clasName="header">Todo List</h1>
+    <TodoList todos={todos} toggleTodo={toggleTodo} deleteTodo={deleteTodo} />
+ 
+  </>
+  )
+}
